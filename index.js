e7515b23b72db9196f4f6ef98dfd081a8a699e5e
Wash1218 <riosortizwashington@gmail.com>
2025-02-18 12:24:01 -0500

    index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const nombreGuardado = localStorage.getItem('nombreUsuario');
 if (nombreGuardado) {
   nombreUsuarioSpan.textContent = nombreGuardado;
 } else {
-  // Si no hay un nombre guardado, redirigir al registro
+  
   window.location.href = 'registro.html';
 }
